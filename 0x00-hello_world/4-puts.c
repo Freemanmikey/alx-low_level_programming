66013417e716539f027fa408d116459376799bf0
freemanmikey <michaelmuisyomark@gmail.com>
2023-12-18 03:34:23 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-/** 
- * main - This is the main function
- *
- * Return: always 0;
- **/
-
-int main(void)
+int main()
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
 }
+
