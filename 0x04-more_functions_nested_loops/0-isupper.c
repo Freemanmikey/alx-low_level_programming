30377cc5a99c060a2398a4b945e3610ccf192867
freemanmikey <michaelmuisyomark@gmail.com>
2023-12-21 15:44:00 -0800

    supper commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * Return: 1 for upper case or 0 for the latter
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
+
